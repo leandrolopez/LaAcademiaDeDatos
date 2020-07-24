d79dc07161669a7c0fe317c43bb9f1f623724c18
LeandroLopez <aleandros@live.com>
2020-07-24 11:47:57 -0400

    Update bubble_sort.py

--- Python/Ejercicios de Python/bubble_sort.py
+++ Python/Ejercicios de Python/bubble_sort.py
@@ -1,22 +1,23 @@
 
 # Declaramos la funcion
-def bubbleSort(nlist):
-    #!Importante! aqui buscamos el maximo numero de la lista menos uno porque no hay otro numero luego de este.
-    for passnum in range(len(nlist)-1,0,-1):
-        # Creamos otro iterador que pasa sobre en rango de la lista
-        for i in range(passnum):
-            # Aqui comparamos el valor de la izquierda con el de la derecha. Si es mayor lo ponemos en la variable temporal 
-            # y en las lineas de codigo 12 y 13 los intercambiamos.  
-            if nlist[i] > nlist[i+1]:
-                temp = nlist[i]
-                nlist[i] = nlist[i+1]
-                nlist[i+1] = temp
 
-# Lista desorganizada
-nlist = [80, 1007, 13,16,53,71,42,61,45,20,70]
+def bubbleSort(nlist):
+    #! Importante! Aqui buscamos el numero maximo de la lista menos uno porque no hay otro numero luego de este
+    for passnum in range(len(nlist)-1,0,-1):
+        # Creamos otro iterador que pasa sobre el rango de la lista
+        for i in range(passnum):
+            #Aqui comparamos el valor de la izquierda con el de la derecha. Si es mayor lo ponemos en la variable temporal
+            # y en las otras lineas de abajo los intercambiamos
+            if nlist[i] > nlist[i + 1]:
+                temp = nlist[i]
+                nlist[i] = nlist[i + 1]
+                nlist[i + 1] = temp
+
+# Lista de numeros desorganizados
+nlist = [80,1007,13,92,16,0, 1, 53,71,42,61,45,20,70]
 
 # Llama a la funcion
 bubbleSort(nlist)
 
-# Imprime el resultado una vez que la funcion complete.
+# Imprime la lista que ahora tendra los numeros organizados
 print(nlist)
